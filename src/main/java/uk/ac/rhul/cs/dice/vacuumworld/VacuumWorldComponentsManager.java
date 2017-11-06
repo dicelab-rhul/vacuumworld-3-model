f11f19eb239900a01ffb9f53a52854ae9eb960e9
cloudstrife9999 <Emanuele.Uliana.2016@live.rhul.ac.uk>
2017-11-06 11:51:28 +0000

    Started implementing the universe.

---
+++ src/main/java/uk/ac/rhul/cs/dice/vacuumworld/VacuumWorldComponentsManager.java
@@ -0,0 +1,12 @@
+package uk.ac.rhul.cs.dice.vacuumworld;
+
+import java.util.concurrent.ExecutorService;
+
+public class VacuumWorldComponentsManager {
+    private ExecutorService executor;
+    private VacuumWorldUniverse universe;
+    
+    public VacuumWorldComponentsManager() {
+	// TODO Auto-generated constructor stub
+    }
+}
--- src/main/java/uk/ac/rhul/cs/dice/vacuumworld/VacuumWorldUniverse.java
+++ src/main/java/uk/ac/rhul/cs/dice/vacuumworld/VacuumWorldUniverse.java
@@ -1,11 +1,18 @@
 package uk.ac.rhul.cs.dice.vacuumworld;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import uk.ac.rhul.cs.dice.agentcontainers.abstractimpl.AbstractUniverse;
+import uk.ac.rhul.cs.dice.vacuumworld.actors.VacuumWorldCleaningAgent;
+import uk.ac.rhul.cs.dice.vacuumworld.actors.VacuumWorldUserAgent;
 import uk.ac.rhul.cs.dice.vacuumworld.appearances.VacuumWorldUniverseAppearance;
 import uk.ac.rhul.cs.dice.vacuumworld.environment.VacuumWorldEnvironment;
+import uk.ac.rhul.cs.dice.vacuumworld.environment.VacuumWorldLocation;
 
 public class VacuumWorldUniverse extends AbstractUniverse {
-
+    private VacuumWorldEnvironment environment;
+    
     public VacuumWorldUniverse(VacuumWorldUniverseAppearance appearance) {
 	super(appearance);
     }
@@ -16,5 +23,23 @@ public class VacuumWorldUniverse extends AbstractUniverse {
     
     public VacuumWorldEnvironment getEnvironment() {
 	return (VacuumWorldEnvironment) getMainAmbient();
+    }
+    
+    private void createEnvironment() {
+	//TODO
+    }
+    
+    public Set<VacuumWorldCleaningAgent> getAllCleaningAgents() {
+	Set<VacuumWorldCleaningAgent> agents = new HashSet<>();
+	this.environment.getGrid().values().stream().filter(VacuumWorldLocation::containsACleaningAgent).map(VacuumWorldLocation::getAgentIfAny).forEach(agents::add);
+	
+	return agents;
+    }
+    
+    public Set<VacuumWorldUserAgent> getAllUsers() {
+	Set<VacuumWorldUserAgent> users = new HashSet<>();
+	this.environment.getGrid().values().stream().filter(VacuumWorldLocation::containsAUser).map(VacuumWorldLocation::getUserIfAny).forEach(users::add);
+	
+	return users;
     }
 }

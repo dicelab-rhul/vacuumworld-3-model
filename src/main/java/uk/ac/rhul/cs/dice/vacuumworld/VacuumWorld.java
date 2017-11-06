701c2b44a05e45983183c69aa5496691032ebae4
cloudstrife9999 <cloudstrife9999@tiscali.it>
2017-11-06 00:03:17 +0000

    Minor.

--- src/main/java/uk/ac/rhul/cs/dice/vacuumworld/VacuumWorld.java
+++ src/main/java/uk/ac/rhul/cs/dice/vacuumworld/VacuumWorld.java
@@ -1,16 +1,10 @@
 package uk.ac.rhul.cs.dice.vacuumworld;
 
-import uk.ac.rhul.cs.dice.agentcontainers.enums.Orientation;
-
 public class VacuumWorld {
 
     private VacuumWorld() {}
 
     public static void main(String[] args) {
-	System.out.println(Orientation.NORTH);
-	System.out.println(Orientation.NORTH.getLeft());
-	System.out.println(Orientation.NORTH.getLeft().getLeft());
-	System.out.println(Orientation.NORTH.getLeft().getLeft().getLeft());
-	System.out.println(Orientation.NORTH.getLeft().getLeft().getLeft().getLeft());
+	//TODO
     }
 }

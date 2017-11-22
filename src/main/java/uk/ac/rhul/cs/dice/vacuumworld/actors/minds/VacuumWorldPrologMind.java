217b27a5fed4616075cd7553d4ae2537368875e1
Kostas Stathis <kostas.stathis@googlemail.com>
2017-11-22 16:51:17 +0000

    "$COMMENT"

---
+++ src/main/java/uk/ac/rhul/cs/dice/vacuumworld/actors/minds/VacuumWorldPrologMind.java
@@ -0,0 +1,46 @@
+package uk.ac.rhul.cs.dice.vacuumworld.actors.minds;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import alice.tuprolog.Prolog;
+import alice.tuprolog.Theory;
+import uk.ac.rhul.cs.dice.agent.interfaces.PrologMind;
+
+public class VacuumWorldPrologMind extends VacuumWorldAbstractMind implements PrologMind {
+	private static final long serialVersionUID = -6107803494980916586L;
+	private Prolog interpreter;
+	private List<Theory> theories;
+	
+	public VacuumWorldPrologMind(String bodyId, Prolog interpreter) {
+		super(bodyId);
+		
+		this.theories = new ArrayList<>();
+		setInterpreter(interpreter);
+	}
+
+	@Override
+	public Prolog getInterpreter() {
+		return this.interpreter;
+	}
+
+	@Override
+	public void setInterpreter(Prolog interpreter) {
+		this.interpreter = interpreter;
+	}
+
+	@Override
+	public List<Theory> getTheories() {
+		return this.theories;
+	}
+
+	@Override
+	public void setTheories(List<Theory> theories) {
+		this.theories = theories;
+	}
+	
+	@Override
+	public Theory getFirstTheory() {		
+		return this.theories != null ? PrologMind.super.getFirstTheory() : null;
+	}
+}

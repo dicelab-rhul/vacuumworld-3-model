34babc805e1d18cd0820a5d3dd40fd3a4260d4da
cloudstrife9999 <Emanuele.Uliana.2016@live.rhul.ac.uk>
2017-11-23 11:26:41 +0000

    Bugfix.

--- src/main/java/uk/ac/rhul/cs/dice/vacuumworld/perception/StopPerception.java
+++ src/main/java/uk/ac/rhul/cs/dice/vacuumworld/perception/StopPerception.java
@@ -5,5 +5,7 @@ import uk.ac.rhul.cs.dice.agent.interfaces.Perception;
 public class StopPerception implements Perception {
     private static final long serialVersionUID = -8310587123628439236L;
     
-    private StopPerception() {}
+    public StopPerception() {
+	//required explicit.
+    }
 }

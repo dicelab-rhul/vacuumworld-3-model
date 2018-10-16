4aaf4795f0aaae2fc555c101fa1f94e7c2dc9fd1
cloudstrife9999 <Emanuele.Uliana.2016@live.rhul.ac.uk>
2018-10-16 16:08:00 +0100

    Updated Tutorial1AgentMind.

--- src/main/java/uk/ac/rhul/cs/dice/vacuumworld/actors/minds/Tutorial1AgentMind.java
+++ src/main/java/uk/ac/rhul/cs/dice/vacuumworld/actors/minds/Tutorial1AgentMind.java
@@ -1,6 +1,7 @@
 package uk.ac.rhul.cs.dice.vacuumworld.actors.minds;
 
 import uk.ac.rhul.cs.dice.agentcontainers.enums.Orientation;
+import uk.ac.rhul.cs.dice.agentcontainers.interfaces.Coordinates;
 import uk.ac.rhul.cs.dice.vacuumworld.actions.VacuumWorldAbstractAction;
 import uk.ac.rhul.cs.dice.vacuumworld.actions.VacuumWorldMoveAction;
 import uk.ac.rhul.cs.dice.vacuumworld.actions.VacuumWorldIdleAction;
@@ -8,23 +9,38 @@ import uk.ac.rhul.cs.dice.vacuumworld.actions.VacuumWorldTurnLeftAction;
 import uk.ac.rhul.cs.dice.vacuumworld.actions.VacuumWorldTurnRightAction;
 import uk.ac.rhul.cs.dice.vacuumworld.actors.minds.VacuumWorldAbstractMind;
 
+/**
+ * 
+ * Tutorial 1 Mind Task 1. You should make a copy of this mind (keep it for
+ * reference) and edit the {@link Tutorial1AgentMind#decide()} method as the
+ * task instructs (see the method below).
+ * 
+ * @author Ben Wilkins
+ *
+ */
 public class Tutorial1AgentMind extends VacuumWorldAbstractMind {
-
     private static final long serialVersionUID = 8029125006026831040L;
+    private Coordinates position = null;
 
+    /**
+     * Constructor. You may do some set up steps here, though they are not necessary
+     * in for this task.
+     * 
+     * @param bodyId
+     *                   the id of this agent.
+     */
     public Tutorial1AgentMind(String bodyId) {
 	super(bodyId);
     }
 
     /**
-     * This is the agents decide method. You should modify the agents behavior in
-     * this methods where indicated. The agents current behavior is to: <br>
+     * This is the agent's decide method. You should modify the agent's behaviour in
+     * this method where indicated. The agent's current behaviour is to: <br>
      * <p>
      * <ul>
-     * <li>1. Attempt a sense action on the first cycle step.
-     * <li>2. Turn to face {@link Orientation#East East} or {@link Orientation#South
+     * <li>1. Turn to face {@link Orientation#East East} or {@link Orientation#South
      * South}
-     * <li>3. Move forward
+     * <li>2. Move forward
      * </ul>
      * <p>
      * 
@@ -40,33 +56,51 @@ public class Tutorial1AgentMind extends VacuumWorldAbstractMind {
      */
     @Override
     public VacuumWorldAbstractAction decide() {
+	// ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** *****//
+
+	// Think about how to find the edge of the grid in an optimal way (i.e) with the
+	// least number of actions. When you have found the wall, indicate this by
+	// performing a notable sequence of actions (e.g. a 360 turn), or by printing
+	// to the console.
+
+	// hint 1: what orientation should the agent have before moving?
+	// hint 2: you can check if a wall is in front of the agent using
+	// isWallForward().
+
 	// ***** ***** ***** ***** ***** EDIT HERE ***** ***** ***** ***** *****//
 
-	// think about how to find the edge of the grid in an optimal way (i.e) with the
-	// least number of actions.
-	// hint 1: what orientation should the agent have before moving?
-	// hint 2: you can get the current coordinates of the agent by using the method
-	// - getCoordinates()
+	// It is best to make a copy of this mind and edit there (otherwise a future git
+	// update might overwrite what you have done!)
 
-	// FOR STUDENTS: YOUR TASK IS TO CHANGE THIS DEFAULT BEHAVIOR!
+	// FOR STUDENTS: YOUR TASK IS TO CHANGE THIS DEFAULT BEHAVIOUR!
 
-	// ***** ***** ***** ***** ***** **** **** ***** ***** ***** ***** *****//
 	if (isOrientationEast() || isOrientationSouth()) {
 	    return new VacuumWorldMoveAction();
 	}
-	else if (isOrientationNorth()) {
+	
+	if (isOrientationNorth()) {
 	    return new VacuumWorldTurnRightAction();
 	}
-	else if (isOrientationWest()) {
+	
+	if (isOrientationWest()) {
 	    return new VacuumWorldTurnLeftAction();
 	}
-	else {
-	    throw new IllegalStateException("WE ARE NOT FACING ANYWHERE!?!?");
-	}
+	
+	return new VacuumWorldIdleAction(); // the default action (which will never happen).
+	// ***** ***** ***** ***** ***** **** **** ***** ***** ***** ***** *****//
     }
 
+    /**
+     * This method is called before decide, it should be used to update the internal
+     * state of the agent. In this case, the {@link Tutorial1AgentMind#position
+     * position} of the agent is updated.
+     */
     @Override
     public void revise() {
-	// Edit here.
+	this.position = getCoordinates();
+    }
+    
+    public Coordinates getPositionCoordinates() {
+        return this.position;
     }
 }

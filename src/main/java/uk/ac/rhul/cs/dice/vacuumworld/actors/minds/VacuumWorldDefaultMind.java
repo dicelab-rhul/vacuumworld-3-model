4379eabe1fe922dc9f1296d95a921033da357887
cloudstrife9999 <Emanuele.Uliana.2016@live.rhul.ac.uk>
2018-10-23 19:04:05 +0100

    Reverted VacuumWorldDefaultMind to its before-bugfix status.

--- src/main/java/uk/ac/rhul/cs/dice/vacuumworld/actors/minds/VacuumWorldDefaultMind.java
+++ src/main/java/uk/ac/rhul/cs/dice/vacuumworld/actors/minds/VacuumWorldDefaultMind.java
@@ -1,30 +1,17 @@
 package uk.ac.rhul.cs.dice.vacuumworld.actors.minds;
 
 import uk.ac.rhul.cs.dice.vacuumworld.actions.VacuumWorldAbstractAction;
-import uk.ac.rhul.cs.dice.vacuumworld.actions.VacuumWorldBroadcastingAction;
-import uk.ac.rhul.cs.dice.vacuumworld.actions.messages.VacuumWorldMessage;
 
 public class VacuumWorldDefaultMind extends VacuumWorldAbstractMind {
     private static final long serialVersionUID = 8611021479667623569L;
-    private int cycle;
     
     public VacuumWorldDefaultMind(String bodyId) {
 	super(bodyId);
-	
-	this.cycle = 0;
     }
 
     @Override
     public VacuumWorldAbstractAction decide() {
-	//return super.decideRandomly();
-	this.cycle++;
-	
-	if(!getMessages().isEmpty()) {
-	    System.out.println("Hello! Cycle #" + this.cycle + ". I am " + getBodyId() + ", and I received messages: " + getMessages().size());
-	}
-	
-	return new VacuumWorldBroadcastingAction(new VacuumWorldMessage(String.valueOf(this.cycle)));
-	
+	return super.decideRandomly();
     }
     
     @Override

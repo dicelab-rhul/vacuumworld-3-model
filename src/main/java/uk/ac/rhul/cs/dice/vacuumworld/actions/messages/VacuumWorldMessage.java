db967162afaed15d3f48e80fb6c8461f2d86b198
cloudstrife9999 <Emanuele.Uliana.2016@live.rhul.ac.uk>
2018-10-30 18:20:57 +0000

    Added JavaDoc.

--- src/main/java/uk/ac/rhul/cs/dice/vacuumworld/actions/messages/VacuumWorldMessage.java
+++ src/main/java/uk/ac/rhul/cs/dice/vacuumworld/actions/messages/VacuumWorldMessage.java
@@ -1,13 +1,33 @@
 package uk.ac.rhul.cs.dice.vacuumworld.actions.messages;
 
+/**
+ * 
+ * Implementation of {@link VacuumWorldMessageInterface}.<br /><br />
+ * A {@link StringBuilder} is used internally to maintain a representation of the current text for efficiency.
+ * 
+ * @author cloudstrife9999
+ *
+ */
 public class VacuumWorldMessage implements VacuumWorldMessageInterface {
     private static final long serialVersionUID = -8042200960462345055L;
     private StringBuilder builder;
 
+    /**
+     * 
+     * Constructs a new {@link VacuumWorldMessage} with an empty {@link String} as text.
+     * 
+     */
     public VacuumWorldMessage() {
 	this("");
     }
 
+    /**
+     * 
+     * Constructs a new {@link VacuumWorldMessage} with an <code>text</code> as text.
+     * 
+     * @param text the text of the message.
+     * 
+     */
     public VacuumWorldMessage(CharSequence text) {
 	this.builder = new StringBuilder(text);
     }
@@ -39,6 +59,13 @@ public class VacuumWorldMessage implements VacuumWorldMessageInterface {
 	this.builder.append(toAppend);
     }
     
+    /**
+     * 
+     * Concatenates the {@link String} "Message: " with the text of the message, and returns the result.
+     * 
+     * @return the result of the concatenation of "Message: " and the text of the message.
+     * 
+     */
     @Override
     public String toString() {
 	return "Message: " + getText();
--- src/main/java/uk/ac/rhul/cs/dice/vacuumworld/actions/messages/VacuumWorldMessageInterface.java
+++ src/main/java/uk/ac/rhul/cs/dice/vacuumworld/actions/messages/VacuumWorldMessageInterface.java
@@ -2,10 +2,58 @@ package uk.ac.rhul.cs.dice.vacuumworld.actions.messages;
 
 import java.io.Serializable;
 
+/**
+ * 
+ * Interface for messages in VacuumWorld.<br /><br />
+ * Known subclasses: {@link VacuumWorldMessage}.
+ * 
+ * @author cloudstrife9999
+ *
+ */
 public interface VacuumWorldMessageInterface extends Serializable {
+    
+    /**
+     * 
+     * Returns the text of the message as a {@link String}. The returned value cannot be <code>null</code>. At the very least it consists of an empty {@link String}.
+     * 
+     * @return the text of the message as a {@link String}.
+     * 
+     */
     public abstract String getText();
+    
+    /**
+     * 
+     * Discards whatever is the current text, and replaces it with <code>text</code>.
+     * 
+     * @param text the new text.
+     * 
+     */
     public abstract void setText(CharSequence text);
+    
+    /**
+     * 
+     * Appends <code>toAppend</code> to the current text.
+     * 
+     * @param toAppend the text to append.
+     * 
+     */
     public abstract void appendAsIs(CharSequence toAppend);
+    
+    /**
+     * 
+     * Appends a space to the current text, and then appends <code>toAppend</code>.
+     * 
+     * @param toAppend the text to append.
+     * 
+     */
     public abstract void appendWithSpaceBefore(CharSequence toAppend);
+    
+    /**
+     * 
+     * Appends a newline ('\n') to the current text, and then appends <code>toAppend</code>.
+     * 
+     * @param toAppend the text to append.
+     * 
+     */
     public abstract void appendWithNewLineBefore(CharSequence toAppend);
 }

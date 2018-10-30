38a2c1305b85685fcb339a1a6ee9a298e6da7076
cloudstrife9999 <Emanuele.Uliana.2016@live.rhul.ac.uk>
2018-10-30 20:22:50 +0000

    Minor.

--- src/main/java/uk/ac/rhul/cs/dice/vacuumworld/perception/NothingMoreIncomingPerception.java
+++ src/main/java/uk/ac/rhul/cs/dice/vacuumworld/perception/NothingMoreIncomingPerception.java
@@ -4,4 +4,13 @@ import uk.ac.rhul.cs.dice.agent.interfaces.Perception;
 
 public class NothingMoreIncomingPerception implements Perception {
     private static final long serialVersionUID = 5341731631677759471L;
+    private String info;
+    
+    public NothingMoreIncomingPerception() {
+	this.info = "Nothing more incoming.";
+    }
+    
+    public String getInfo() {
+	return this.info;
+    }
 }
--- src/main/java/uk/ac/rhul/cs/dice/vacuumworld/perception/StopPerception.java
+++ src/main/java/uk/ac/rhul/cs/dice/vacuumworld/perception/StopPerception.java
@@ -4,4 +4,13 @@ import uk.ac.rhul.cs.dice.agent.interfaces.Perception;
 
 public class StopPerception implements Perception {
     private static final long serialVersionUID = -8310587123628439236L;
+    private String stopMessage;
+    
+    public StopPerception() {
+	this.stopMessage = "STOP!";
+    }
+    
+    public String getStopMessage() {
+	return this.stopMessage;
+    }
 }

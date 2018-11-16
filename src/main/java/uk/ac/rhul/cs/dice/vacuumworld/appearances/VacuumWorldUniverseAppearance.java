4bcb79821389f9b8bd4b953a326bc9360c90c7fe
cloudstrife9999 <Emanuele.Uliana.2016@live.rhul.ac.uk>
2018-11-16 12:20:59 +0000

    Minor.

--- src/main/java/uk/ac/rhul/cs/dice/vacuumworld/appearances/VacuumWorldUniverseAppearance.java
+++ src/main/java/uk/ac/rhul/cs/dice/vacuumworld/appearances/VacuumWorldUniverseAppearance.java
@@ -4,8 +4,13 @@ import uk.ac.rhul.cs.dice.agentcontainers.interfaces.UniverseAppearance;
 
 public class VacuumWorldUniverseAppearance implements UniverseAppearance {
     private static final long serialVersionUID = 1173699153569046653L;
+    private boolean initialized;
     
     public VacuumWorldUniverseAppearance() {
-	// Useless for now.
+	this.initialized = true;
+    }
+    
+    public boolean isInitialized() {
+	return this.initialized;
     }
 }
